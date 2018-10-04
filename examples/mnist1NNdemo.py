de5796936dc3c2bd421c56387845f8e7a0b1670e
chofchof <chof@nims.re.kr>
2018-10-04 15:40:02 +0900

    Retouch details

--- examples/mnist1NNdemo.py
+++ examples/mnist1NNdemo.py
@@ -2,17 +2,19 @@
 
 import numpy as np
 
-default_test_size = -1
-default_bucket_size = 1000
+DEFAULT_TEST_SIZE = None
+DEFAULT_BUCKET_SIZE = 1000
+MINIMUM_BUCKET_SIZE = 100
+
 
 def PredictandError(trainims, trainlabs, testims, testlabs):
+
     from sklearn.metrics.pairwise import euclidean_distances
     Distances = euclidean_distances(trainims, testims, squared=True)
 
     predictions = trainlabs[np.argmin(Distances, axis=0)]
+    return np.not_equal(predictions, testlabs).sum()
 
-    error = 1 - np.mean(np.equal(predictions, testlabs))
-    return (error * 100)
 
 def run_main(TestSize, BucketSize):
 
@@ -22,7 +24,7 @@ def run_main(TestSize, BucketSize):
     TrainIms = TrainIms.reshape(TrainIms.shape[0], -1)
     TestIms = TestIms.reshape(TestIms.shape[0], -1)
 
-    if TestSize == default_test_size:
+    if TestSize is DEFAULT_TEST_SIZE:
         TestSize = len(TrainLabels)
     BucketSize = min(BucketSize, TestSize)
 
@@ -32,7 +34,9 @@ def run_main(TestSize, BucketSize):
     # error: 3.0900000000000025
     # Time taken: 19.416749477386475
     errors = [
-        PredictandError(TrainIms, TrainLabels, TestIms[i:(i+BucketSize)], TestLabels[i:(i+BucketSize)])
+        PredictandError(TrainIms, TrainLabels,
+                        TestIms[i:min(i+BucketSize, TestSize)],
+                        TestLabels[i:min(i+BucketSize, TestSize)])
         for i in range(0, TestSize, BucketSize)
     ]
 
@@ -47,25 +51,29 @@ def run_main(TestSize, BucketSize):
 
     t1 = time.time()
 
-    print("error: {}".format(np.mean(errors)))
-    print("Time taken: {}".format(t1 - t0))
+    print("error: {:.3f} %".format(sum(errors) / TestSize * 100))
+    print("Time taken: {:.3f} sec".format(t1 - t0))
+
 
 def parse_args():
 
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('--test-size', type=int, default=default_test_size,
+
+    parser.add_argument('--test-size', type=int, default=DEFAULT_TEST_SIZE,
                         help='size of the test images')
-    parser.add_argument('--bucket-size', type=int, default=default_bucket_size,
+    parser.add_argument('--bucket-size', type=int, default=DEFAULT_BUCKET_SIZE,
                         help='size of the test bucket')
     args = parser.parse_args()
-    
-    assert (args.test_size == default_test_size or args.test_size >= default_bucket_size), \
-        'Too small test size (< {}).'.format(default_bucket_size)
-    assert (args.bucket_size >= 100), 'Too small bucket size (< 100).'
-    
+
+    assert (args.test_size is DEFAULT_TEST_SIZE or args.test_size >= DEFAULT_BUCKET_SIZE), \
+        'Too small test size (< {}).'.format(DEFAULT_BUCKET_SIZE)
+    assert (args.bucket_size >= MINIMUM_BUCKET_SIZE), \
+        'Too small bucket size (< {}).'.format(MINIMUM_BUCKET_SIZE)
+
     return args.test_size, args.bucket_size
-    
+
+
 if __name__ == '__main__':
     test_size, bucket_size = parse_args()
     run_main(test_size, bucket_size)
