d7dd6e50ba1a45cb0701cc1007ce1857aeeff001
chofchof <chof@nims.re.kr>
2018-10-04 11:54:55 +0900

    Use PyCharm

--- examples/mnist1NNdemo.py
+++ examples/mnist1NNdemo.py
@@ -2,23 +2,28 @@
 
 import numpy as np
 
-from keras.datasets import mnist
-(TrainIms, TrainLabels), (TestIms, TestLabels) = mnist.load_data()
+default_test_size = -1
+default_bucket_size = 1000
 
-TrainIms = TrainIms.reshape(TrainIms.shape[0], -1)
-TestIms = TestIms.reshape(TestIms.shape[0], -1)
-
-def PredictandError(testims, testlabs):
+def PredictandError(trainims, trainlabs, testims, testlabs):
     from sklearn.metrics.pairwise import euclidean_distances
-    Distances = euclidean_distances(TrainIms, testims, squared=True)
+    Distances = euclidean_distances(trainims, testims, squared=True)
 
-    predictions = TrainLabels[np.argmin(Distances, axis=0)]
+    predictions = trainlabs[np.argmin(Distances, axis=0)]
 
     error = 1 - np.mean(np.equal(predictions, testlabs))
     return (error * 100)
 
 def run_main(TestSize, BucketSize):
 
+    from keras.datasets import mnist
+    (TrainIms, TrainLabels), (TestIms, TestLabels) = mnist.load_data()
+
+    TrainIms = TrainIms.reshape(TrainIms.shape[0], -1)
+    TestIms = TestIms.reshape(TestIms.shape[0], -1)
+
+    if TestSize == default_test_size:
+        TestSize = len(TrainLabels)
     BucketSize = min(BucketSize, TestSize)
 
     import time
@@ -26,8 +31,10 @@ def run_main(TestSize, BucketSize):
 
     # error: 3.0900000000000025
     # Time taken: 19.416749477386475
-    errors = [PredictandError(TestIms[i:(i+BucketSize)], TestLabels[i:(i+BucketSize)])
-              for i in range(0, TestSize, BucketSize)]
+    errors = [
+        PredictandError(TrainIms, TrainLabels, TestIms[i:(i+BucketSize)], TestLabels[i:(i+BucketSize)])
+        for i in range(0, TestSize, BucketSize)
+    ]
 
     # error: 3.090000000000004
     # Time taken: 80.45218276977539    
@@ -45,9 +52,6 @@ def run_main(TestSize, BucketSize):
 
 def parse_args():
 
-    default_test_size = len(TestLabels)
-    default_bucket_size = 1000
-    
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('--test-size', type=int, default=default_test_size,
@@ -56,7 +60,7 @@ def parse_args():
                         help='size of the test bucket')
     args = parser.parse_args()
     
-    assert (args.test_size >= default_bucket_size), \
+    assert (args.test_size == default_test_size or args.test_size >= default_bucket_size), \
         'Too small test size (< {}).'.format(default_bucket_size)
     assert (args.bucket_size >= 100), 'Too small bucket size (< 100).'
     
